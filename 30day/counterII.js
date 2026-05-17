6f6a8457bcfcd583de407133be46e7fdf67c325b
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2026-05-17 16:57:35 -0300

    Create counterII.js

---
+++ 30day/counterII.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const valorInicial = init;
+    return{
+        increment: function(){
+            return(init = init + 1)
+
+        },
+        decrement: function(){
+            return(init = init - 1)
+
+        },
+        reset: function(){
+            init = valorInicial
+            return(init)
+        }
+
+    };
+    
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
